ddbdc26ae991923505803565bb4bd7e00df2e70c
kylieogg <kylieogg1@gmail.com>
2022-06-29 11:07:58 -0400

    updated comments in script file

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 // Does moment need additional install?
 
 // Load current date at bottom of header
+
+// Color-code time blocks for past, present, or future
+
+// On time block click, events can be entered and saved in local storage. Upon page refresh, saved events persist.
