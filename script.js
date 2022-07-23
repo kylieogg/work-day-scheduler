d558d869ec9d90adddcfe89631d8e106391b39d9
kylieogg <kylieogg1@gmail.com>
2022-07-23 14:48:10 -0400

    making sure current code is saved

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let today = moment();
 $("#currentDay").text(today.format("MMMM Do YYYY"));
 
+
 $(document).ready(function () {
     // Click event listener for save button
     $(".saveBtn").on("click", function () {
@@ -12,34 +13,21 @@ $(document).ready(function () {
         localStorage.setItem(time, text);
     }) 
 
-    function trackTime() {
-        // Gets current time
-        var currentTime = moment().hour();
+       
+    // Get items from local storage
+    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
-        $("")
-    }        
 })
 
 
-
-
-
-// Color-code time blocks for past, present, or future
-
-
-
-
-
-// Get items from local storage
-$('#hour-9 .description').val(localStorage.getItem('hour-9'));
-$('#hour-10 .description').val(localStorage.getItem('hour-10'));
-$('#hour-11 .description').val(localStorage.getItem('hour-11'));
-$('#hour-12 .description').val(localStorage.getItem('hour-12'));
-$('#hour-13 .description').val(localStorage.getItem('hour-13'));
-$('#hour-14 .description').val(localStorage.getItem('hour-14'));
-$('#hour-15 .description').val(localStorage.getItem('hour-15'));
-$('#hour-16 .description').val(localStorage.getItem('hour-16'));
-$('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
 
 
